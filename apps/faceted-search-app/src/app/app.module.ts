3f433a46293a9f75bc2da9cbd5b5296a23a76d99
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-01-27 14:33:32 +0100

    chore: install material

--- apps/faceted-search-app/src/app/app.module.ts
+++ apps/faceted-search-app/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FacetSearchModule } from '@faceted-search/facet-search';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FacetSearchModule],
+  imports: [BrowserModule, FacetSearchModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
