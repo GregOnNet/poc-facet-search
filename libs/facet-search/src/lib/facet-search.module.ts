fc8762b38580143f05dbde3bfb83f2fb8b9d48ad
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-01-29 13:36:52 +0100

    ref: remove material components

--- libs/facet-search/src/lib/facet-search.module.ts
+++ libs/facet-search/src/lib/facet-search.module.ts
@@ -1,10 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { FacetBrickComponent } from './facet-brick.component';
 import { FacetBricksComponent } from './facet-bricks.component';
 import { FacetLabelsPipe } from './facet-labels.pipe';
@@ -12,14 +8,7 @@ import { FacetSearchComponent } from './facet-search.component';
 import { FacetValueLabelPipe } from './facet-value-label.pipe';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatAutocompleteModule,
-    MatChipsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [
     FacetBrickComponent,
     FacetBricksComponent,
